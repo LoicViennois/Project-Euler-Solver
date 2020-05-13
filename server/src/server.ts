9c1b4d09fb35c68f4792959608240d7091845a04
Loïc Viennois <contact@loicviennois.com>
2020-05-13 23:53:12 +0200

    fix(server): gracefully shutdown server on SIGINT and SIGTERM signals

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,17 @@ import { app } from './app'
 app.set('port', process.env.EXPRESS_PORT || 3000)
 app.set('interface', process.env.EXPRESS_INTERFACE || '0.0.0.0')
 
-app.listen(app.get('port'), app.get('interface'), () => {
+const server = app.listen(app.get('port'), app.get('interface'), () => {
   console.log(`express app listening on port ${app.get('port')} via interface ${app.get('interface')}`)
 })
+
+process.on('SIGINT', shutdownServer)
+
+process.on('SIGTERM', shutdownServer)
+
+function shutdownServer () {
+  console.info('shutting down...')
+  server.close(() => {
+    console.info('bye')
+  })
+}
