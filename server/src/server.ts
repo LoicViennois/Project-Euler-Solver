0c0f9c285e618871bb8ad12f696e4a845aac7475
Loïc Viennois <contact@loicviennois.com>
2020-05-29 22:07:17 +0200

    build: add multistage docker files with dev mode

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app'
 
 app.set('port', process.env.EXPRESS_PORT || 3000)
-app.set('interface', process.env.EXPRESS_INTERFACE || '0.0.0.0')
+app.set('interface', process.env.EXPRESS_INTERFACE || 'localhost')
 
 const server = app.listen(app.get('port'), app.get('interface'), () => {
   console.log(`express app listening on port ${app.get('port')} via interface ${app.get('interface')}`)
