16da4905e1cb4a222e19faf7e5e541ea9524840a
Loïc Viennois <contact@loicviennois.com>
2021-07-12 13:40:20 +0200

    feat(server): use custom number to words generator and remove npm package number-to-words

--- server/src/problems/001-020/euler017.ts
+++ server/src/problems/001-020/euler017.ts
@@ -1,18 +1,35 @@
-import { range } from '../utils';
-import { toWords } from 'number-to-words';
-
 /*
 https://projecteuler.net/problem=17
 */
 export function euler017(): number {
-  let nbLetters = 0;
-  for (const num of range(1, 1001)) {
-    const words = toWords(num)
-      .replace(/hundred\s/g, 'hundredand')
-      .replace(/\s/g, '')
-      .replace('-', '');
-    nbLetters += words.length;
-  }
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const hundreds = ['', ...units.slice(1).map((unit) => unit + 'hundred')];
 
-  return nbLetters;
+  let allCustomWords: string[] = [];
+  for (const hundred of hundreds) {
+    for (const ten of tens) {
+      for (const unit of units) {
+        allCustomWords.push(hundred + ten + unit);
+      }
+    }
+  }
+  allCustomWords.shift();
+  allCustomWords = allCustomWords.map((val) => {
+    return val.replace(/hundred$/, 'tmp')
+      .replace(/hundred/, 'hundredand')
+      .replace('tmp', 'hundred')
+      .replace('tenone', 'eleven')
+      .replace('tentwo', 'twelve')
+      .replace('tenthree', 'thirteen')
+      .replace('tenfour', 'fourteen')
+      .replace('tenfive', 'fifteen')
+      .replace('tensix', 'sixteen')
+      .replace('tenseven', 'seventeen')
+      .replace('teneight', 'eighteen')
+      .replace('tennine', 'nineteen');
+  });
+  allCustomWords.push('onethousand');
+
+  return allCustomWords.map((w) => w.length).reduce((a, b) => a + b);
 }
