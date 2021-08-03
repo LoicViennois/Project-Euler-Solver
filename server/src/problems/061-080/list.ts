52886d1327a510cd20c7088e26fe12e7f3b6e7c2
Loïc Viennois <contact@loicviennois.com>
2021-08-03 22:23:08 +0200

    fix(server): fix code fetch in production environment

--- server/src/problems/061-080/list.ts
+++ server/src/problems/061-080/list.ts
@@ -1,15 +1,19 @@
 import { euler061 } from './euler061';
 import { euler062 } from './euler062';
 import { euler063 } from './euler063';
+import { euler064 } from './euler064';
+import { euler065 } from './euler065';
+import { euler066 } from './euler066';
+import { euler067 } from './euler067';
 
 export let problems = new Map<number, () => number>([
   [61, euler061],
   [62, euler062],
   [63, euler063],
-  [64, null],
-  [65, null],
-  [66, null],
-  [67, null],
+  [64, euler064],
+  [65, euler065],
+  [66, euler066],
+  [67, euler067],
   [68, null],
   [69, null],
   [70, null],
--- server/src/server/routes/code.ts
+++ server/src/server/routes/code.ts
@@ -5,24 +5,30 @@ export const getCode: RequestHandler = (req: Request, res: Response) => {
   const name: string = req.params.id;
   let filePath: string = null;
 
+  let pathPrefix = 'src';
+  let ext = 'ts';
+  if (process.env.NODE_ENV === 'production') {
+    pathPrefix = 'dist';
+    ext = 'js';
+  }
   console.log(`Get code for ${name}`);
 
   const regex = /^euler(\d{3})$/;
 
   if (name === 'maths' || name === 'utils') {
     // maths or utils
-    filePath = `./src/problems/${name}.ts`;
+    filePath = `./${pathPrefix}/problems/${name}.${ext}`;
   } else if (regex.test(name)) {
     // problems
     const id = +regex.exec(name)[1];
     if (id <= 20) {
-      filePath = `./src/problems/001-020/${name}.ts`;
+      filePath = `./${pathPrefix}/problems/001-020/${name}.${ext}`;
     } else if (id <= 40) {
-      filePath = `./src/problems/021-040/${name}.ts`;
+      filePath = `./${pathPrefix}/problems/021-040/${name}.${ext}`;
     } else if (id <= 60) {
-      filePath = `./src/problems/041-060/${name}.ts`;
+      filePath = `./${pathPrefix}/problems/041-060/${name}.${ext}`;
     } else if (id <= 80) {
-      filePath = `./src/problems/061-080/${name}.ts`;
+      filePath = `./${pathPrefix}/problems/061-080/${name}.${ext}`;
     }
   }
 
