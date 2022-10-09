b8bce4e77852780c51fe38b2fcf3f672d2c165c7
Loïc Viennois <contact@loicviennois.com>
2022-10-09 19:01:59 +0200

    feat: update PWA config

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -39,7 +39,12 @@ import { environment } from '../environments/environment';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [ProblemsService],
   bootstrap: [AppComponent]
