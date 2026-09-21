f5c2940e9a83843da3fd7418157a947b4c412763
Loïc Viennois <contact@loicviennois.com>
2026-09-21 21:20:46 +0200

    test: simplify playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,37 +1,21 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? 'html' : [['html', { open: 'never' }], ['list']],
+  reporter: 'html',
   use: {
     baseURL: 'http://localhost:4200',
+    serviceWorkers: 'block',
+    screenshot: 'only-on-failure',
     trace: 'on-first-retry',
   },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
   webServer: {
     command: 'pnpm start',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    timeout: 120_000,
   },
 });
